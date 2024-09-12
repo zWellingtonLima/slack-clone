49454a70bb12a6bb762922b8c20b40ddaf12fea2
zWellingtonLima <0wellingtonlima1@gmail.com>
2024-09-12 10:16:03 +0100

    refac: removed comment

--- src/middleware.ts
+++ src/middleware.ts
@@ -15,7 +15,6 @@ export default convexAuthNextjsMiddleware((request) => {
   if (isPublicPage(request) && isAuthenticatedNextjs()) {
     return nextjsMiddlewareRedirect(request, "/");
   }
-  // TODO: Redirect user away from "/auth" if authenticated
 });
 
 export const config = {
