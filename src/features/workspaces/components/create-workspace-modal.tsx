1f6bc8a20bbe61f7d46121629658e86346c68d02
zWellingtonLima <0wellingtonlima1@gmail.com>
2024-09-20 12:57:54 +0100

    feat: added create-workspace-modal

---
+++ src/features/workspaces/components/create-workspace-modal.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { useState } from "react";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { useCreateWorkspaceModal } from "../store/use-create-workspace-modal";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { useCreateWorkspace } from "../api/use-create-workspace";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+
+export const CreateWorkspaceModal = () => {
+  const router = useRouter();
+  const [name, setName] = useState("");
+  const [open, setOpen] = useCreateWorkspaceModal();
+
+  const { mutate, isPending } = useCreateWorkspace();
+
+  const handleCloseModal = () => {
+    setOpen(false);
+    setName("");
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    mutate(
+      { name },
+      {
+        onSuccess(id) {
+          toast.success("Workspace created");
+          router.push(`/workspace/${id}`);
+          handleCloseModal();
+        },
+      }
+    );
+  };
+
+  return (
+    <Dialog open={open} onOpenChange={handleCloseModal}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Add a workspace</DialogTitle>
+        </DialogHeader>
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <Input
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            disabled={isPending}
+            required
+            autoFocus
+            minLength={3}
+            placeholder="Workspace name e.g. 'Work', 'Personal', 'Home'"
+          />
+          <div className="flex justify-end">
+            <Button disabled={isPending}>Create</Button>
+          </div>
+        </form>
+      </DialogContent>
+    </Dialog>
+  );
+};
