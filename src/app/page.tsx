a768e3ab4d355d43c6092579f0fce45b5a7ecc59
zWellingtonLima <0wellingtonlima1@gmail.com>
2024-09-20 13:00:52 +0100

    feat: added to main page a side effect handling workspaces id

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,30 @@
 "use client";
 
+import { useEffect, useMemo } from "react";
+
 import { UserButton } from "@/features/auth/components/user-button";
 
+import { useGetWorkspaces } from "@/features/workspaces/api/use-get-workspaces";
+import { useCreateWorkspaceModal } from "@/features/workspaces/store/use-create-workspace-modal";
+import { useRouter } from "next/navigation";
+
 export default function Home() {
+  const [open, setOpen] = useCreateWorkspaceModal();
+
+  const router = useRouter();
+  const { data, isLoading } = useGetWorkspaces();
+  const workspaceId = useMemo(() => data?.[0]?._id, [data]);
+
+  useEffect(() => {
+    if (isLoading) return;
+
+    if (workspaceId) {
+      router.replace(`/workspace/${workspaceId}`)
+    } else if (!open) {
+      setOpen(true);      
+    }
+  }, [workspaceId, isLoading, open, setOpen, router]);
+
   return (
     <div>
       <UserButton />
