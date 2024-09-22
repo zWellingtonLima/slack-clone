af9545323487c782c65a99779364041fa02de50a
zWellingtonLima <0wellingtonlima1@gmail.com>
2024-09-22 11:32:54 +0100

    refac: removed test content

--- src/app/workspace/[workspaceId]/page.tsx
+++ src/app/workspace/[workspaceId]/page.tsx
@@ -1,13 +1,5 @@
-"use client";
-
-import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
-import { useWorkspaceId } from "@/hooks/use-workspace-id";
-
 const WorkspaceIdPage = () => {
-  const workspaceId = useWorkspaceId();
-  const { data } = useGetWorkspace({ id: workspaceId });
-
-  return <div>ID: {workspaceId}</div>;
+  return <div>Workspace Id page</div>;
 };
 
 export default WorkspaceIdPage;
