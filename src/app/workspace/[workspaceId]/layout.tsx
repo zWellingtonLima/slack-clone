8ccc182c34a249a236610e013cd02ad78596e95d
zWellingtonLima <0wellingtonlima1@gmail.com>
2024-09-22 11:34:35 +0100

    feat: added components to layout

--- src/app/workspace/[workspaceId]/layout.tsx
+++ src/app/workspace/[workspaceId]/layout.tsx
@@ -1,5 +1,6 @@
-"use client"
+"use client";
 
+import { Sidebar } from "./sidebar";
 import { Toolbar } from "./toolbar";
 
 interface WorkspaceIdLayoutProps {
@@ -7,10 +8,15 @@ interface WorkspaceIdLayoutProps {
 }
 
 const WorkspaceIdLayout = ({ children }: WorkspaceIdLayoutProps) => {
-  return (<div className="h-full">
-    <Toolbar />
-    {children}
-    </div>)
+  return (
+    <div className="h-full">
+      <Toolbar />
+      <div className="flex h-[calc(100vh-40px)]">
+        <Sidebar />
+        {children}
+      </div>
+    </div>
+  );
 };
 
 export default WorkspaceIdLayout;
