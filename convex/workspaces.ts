f0f779d7702b9905a7372cd1a60f0881b9ff5ff3
zWellingtonLima <0wellingtonlima1@gmail.com>
2024-09-22 12:10:15 +0100

    feat: added insert members when creating a workspace and changed the get workspace to show only workspaces that the user currently are part of

--- convex/workspaces.ts
+++ convex/workspaces.ts
@@ -20,6 +20,12 @@ export const create = mutation({
       joinCode,
     });
 
+    await ctx.db.insert("members", {
+      userId,
+      workspaceId,
+      role: "admin",
+    });
+
     return workspaceId;
   },
 });
@@ -27,7 +33,29 @@ export const create = mutation({
 export const get = query({
   args: {},
   handler: async (ctx) => {
-    return await ctx.db.query("workspaces").collect();
+    const userId = await getAuthUserId(ctx);
+
+    if (!userId) {
+      return [];
+    }
+
+    const members = await ctx.db
+      .query("members")
+      .withIndex("by_user_id", (q) => q.eq("userId", userId))
+      .collect();
+
+    const workspaceIds = members.map((member) => member.workspaceId);
+    const workspaces = [];
+
+    for (const workspaceId of workspaceIds) {
+      const workspace = await ctx.db.get(workspaceId);
+
+      if (workspace) {
+        workspaces.push(workspace);
+      }
+    }
+
+    return workspaces;
   },
 });
 
@@ -40,7 +68,18 @@ export const getById = query({
       throw new Error("Unauthorized");
     }
 
-    return await ctx.db.get(args.id)
+    const member = await ctx.db
+      .query("members")
+      .withIndex("by_workspace_id_user_id", (q) =>
+        q.eq("workspaceId", args.id).eq("userId", userId)
+      )
+      .unique();
+
+    if (!member) {
+      return null;
+    }
+
+    return await ctx.db.get(args.id);
   },
   // TODO: refact after to allow only users that are part of this workspace to fetch its Id
 });
