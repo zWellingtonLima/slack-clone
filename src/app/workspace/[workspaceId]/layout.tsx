424a9ffed8cb48686f612a63bf106ff77b066ddc
zWellingtonLima <0wellingtonlima1@gmail.com>
2024-09-26 13:29:39 +0100

    feat: added workspace sidebar component

--- src/app/workspace/[workspaceId]/layout.tsx
+++ src/app/workspace/[workspaceId]/layout.tsx
@@ -1,7 +1,13 @@
 "use client";
 
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
 import { Sidebar } from "./sidebar";
 import { Toolbar } from "./toolbar";
+import { WorkspaceSidebar } from "./workspace-sidebar";
 
 interface WorkspaceIdLayoutProps {
   children: React.ReactNode;
@@ -13,7 +19,17 @@ const WorkspaceIdLayout = ({ children }: WorkspaceIdLayoutProps) => {
       <Toolbar />
       <div className="flex h-[calc(100vh-40px)]">
         <Sidebar />
-        {children}
+        <ResizablePanelGroup direction="horizontal" autoSaveId="wl-workspace-layout">
+          <ResizablePanel
+            defaultSize={20}
+            minSize={11}
+            className="bg-[#5E2C5F]"
+          >
+            <WorkspaceSidebar />
+          </ResizablePanel>
+          <ResizableHandle withHandle />
+          <ResizablePanel minSize={20}>{children}</ResizablePanel>
+        </ResizablePanelGroup>
       </div>
     </div>
   );
---
+++ src/app/workspace/[workspaceId]/workspace-sidebar.tsx
@@ -0,0 +1,5 @@
+export const WorkspaceSidebar = () => {
+  return (
+    <div>Workspace</div>
+  )
+}
