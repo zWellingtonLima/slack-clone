a83c14bae0cb12dd662b20a5c4a7d0f8aaf3bd0e
zWellingtonLima <0wellingtonlima1@gmail.com>
2024-09-27 13:11:24 +0100

    feat: added workspace header and sidebar

---
+++ src/app/workspace/[workspaceId]/workspace-header.tsx
@@ -0,0 +1,73 @@
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+} from "@radix-ui/react-dropdown-menu";
+
+import { Doc } from "../../../../convex/_generated/dataModel";
+import { ChevronDown, SquarePen } from "lucide-react";
+import {
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+} from "@/components/ui/dropdown-menu";
+
+interface WorkspaceHeaderProps {
+  workspace: Doc<"workspaces">;
+  isAdmin: boolean;
+}
+
+export const WorkspaceHeader = ({
+  workspace,
+  isAdmin,
+}: WorkspaceHeaderProps) => {
+  return (
+    <div className="flex items-center justify-between px-4 h-[49px] gap-0.5">
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button
+            variant="transparent"
+            className="font-semibold text-lg w-auto p-1.5 overflow-hidden"
+            size="sm"
+          >
+            <span className="truncate">{workspace.name}</span>
+            <ChevronDown className="size-4 ml-1 shrink-0" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent side="bottom" align="start" className="w-64">
+          <DropdownMenuItem className="cursor-pointer capitalize">
+            <div className="size-9 relative overflow-hidden bg-[#616061] text-white font-semibold text-xl rounded-md flex items-center justify-center mr-2">
+              {workspace.name.charAt(0).toUpperCase()}
+            </div>
+            <div className="flex flex-col items-start">
+              <p className="font-bold">{workspace.name}</p>
+              <p className="text-xs text-muted-foreground">Active workspace</p>
+            </div>
+          </DropdownMenuItem>
+          {isAdmin && (
+            <>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem
+                className="cursor-pointer py-2"
+                onClick={() => {}}
+              >
+                Invite people to {workspace.name}
+              </DropdownMenuItem>
+              <DropdownMenuItem
+                className="cursor-pointer py-2"
+                onClick={() => {}}
+              >
+                Preferences
+              </DropdownMenuItem>
+            </>
+          )}
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <div className="flex items-center gap-0.5">
+        <Button variant='transparent' size='iconSm'>
+          <SquarePen className="size-4"/>
+        </Button>
+      </div>
+    </div>
+  );
+};
