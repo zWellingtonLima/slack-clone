04e64c91a3eab5e3be55506edffadc57f842792c
zWellingtonLima <0wellingtonlima1@gmail.com>
2024-10-01 12:13:02 +0100

    feat: added update mutation to workspaces api endpoint

--- convex/workspaces.ts
+++ convex/workspaces.ts
@@ -91,3 +91,34 @@ export const getById = query({
   },
   // TODO: refact after to allow only users that are part of this workspace to fetch its Id
 });
+
+export const update = mutation({
+  args: {
+    id: v.id("workspaces"),
+    name: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const userId = await getAuthUserId(ctx);
+
+    if (!userId) {
+      throw new Error("Unauthorized");
+    }
+
+    const member = await ctx.db
+      .query("members")
+      .withIndex("by_workspace_id_user_id", (q) =>
+        q.eq("workspaceId", args.id).eq("userId", userId)
+      )
+      .unique();
+
+    if (!member || member.role !== "admin") {
+      throw new Error("Unauthorized");
+    }
+
+    await ctx.db.patch(args.id, {
+      name: args.name,
+    });
+
+    return args.id;
+  },
+});
