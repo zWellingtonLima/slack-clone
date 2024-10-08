67b73a0045f436c7ea550539f8c2b490fb3683c1
zWellingtonLima <0wellingtonlima1@gmail.com>
2024-10-08 12:10:09 +0100

    feat: added toggle feature

--- src/app/workspace/[workspaceId]/workspace-section.tsx
+++ src/app/workspace/[workspaceId]/workspace-section.tsx
@@ -1,8 +1,10 @@
 import { FaCaretDown } from "react-icons/fa";
+import { useToggle } from "react-use";
+import { PlusIcon } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
 import { Hint } from "@/components/hint";
-import { PlusIcon } from "lucide-react";
+import { cn } from "@/lib/utils";
 
 interface WorkspaceSectionProps {
   children: React.ReactNode;
@@ -17,14 +19,17 @@ export const WorkspaceSection = ({
   hint,
   onNew,
 }: WorkspaceSectionProps) => {
+  const [on, toggle] = useToggle(true);
+
   return (
     <div className="flex flex-col mt-3 px-2">
       <div className="flex items-center px-3.5 group">
         <Button
           variant="transparent"
           className="p-0.5 text-sm text-[#f9edffcc] shrink-0 size-6"
+          onClick={toggle}
         >
-          <FaCaretDown className="size-4" />
+          <FaCaretDown className={cn("size-4 transition-transform", on && '-rotate-90')} />
         </Button>
         <Button
           variant="transparent"
@@ -46,7 +51,7 @@ export const WorkspaceSection = ({
           </Hint>
         )}
       </div>
-      {children}
+      {on && children}
     </div>
   );
 };
