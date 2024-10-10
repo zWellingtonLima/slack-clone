2218025eb2c02de81745591566d2b30b8eb7b056
zWellingtonLima <0wellingtonlima1@gmail.com>
2024-10-10 12:40:33 +0100

    feat: created invite-modal.tsx component

---
+++ src/app/workspace/[workspaceId]/invite-modal.tsx
@@ -0,0 +1,46 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { CopyIcon } from "lucide-react";
+
+interface InviteModalProps {
+  open: boolean;
+  setOpen: (open: boolean) => void;
+  name: string;
+  joinCode: string;
+}
+
+export const InviteModal = ({
+  open,
+  setOpen,
+  name,
+  joinCode,
+}: InviteModalProps) => {
+  const handleCopy = () => {
+    const inviteLink = ``
+  }
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Invite people to {name}</DialogTitle>
+          <DialogDescription>
+            Use the code below to invite people to your workspace
+          </DialogDescription>
+        </DialogHeader>
+        <div className="flex flex-col gap-y-4 items-center justify-center py-10">
+          <p className="text-4xl font-bold tracking-widest uppercase">
+            {joinCode}
+          </p>
+          <Button variant='ghost' size='sm'>Copy link <CopyIcon className="size-4 ml-2"/></Button>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+};
