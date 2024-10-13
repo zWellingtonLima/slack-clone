8a0667b90028fe007607b048eb9e5d852b811f91
zWellingtonLima <0wellingtonlima1@gmail.com>
2024-10-13 13:11:03 +0100

    feat: added join route to app folder and added VerificationInput

---
+++ src/app/join/[workspaceId]/page.tsx
@@ -0,0 +1,88 @@
+"use client";
+import VerificationInput from "react-verification-input";
+import Image from "next/image";
+import Link from "next/link";
+import { Loader } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
+import { useEffect, useMemo } from "react";
+
+import { Button } from "@/components/ui/button";
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
+import { useGetWorkspaceInfo } from "@/features/workspaces/api/use-get-workspace-info";
+import { useJoin } from "@/features/workspaces/api/use-join";
+import { cn } from "@/lib/utils";
+
+const JoinPage = () => {
+  const workspaceId = useWorkspaceId();
+  const router = useRouter();
+
+  const { mutate, isPending } = useJoin();
+  const { data, isLoading } = useGetWorkspaceInfo({ id: workspaceId });
+
+  const isMember = useMemo(() => data?.isMember, [data?.isMember]);
+
+  useEffect(() => {
+    if (isMember) {
+      router.push(`/workspace/${workspaceId}`);
+    }
+  }, [isMember, router, workspaceId]);
+
+  const handleComplete = (value: string) => {
+    mutate(
+      { workspaceId, joinCode: value },
+      {
+        onSuccess: (id) => {
+          router.replace(`/workspace/${id}`);
+          toast.success("Workspace joined.");
+        },
+        onError: () => {
+          toast.error("Failed to join workspace");
+        },
+      }
+    );
+  };
+
+  if (!isLoading) {
+    return (
+      <div className="h-full flex items-center justify-center">
+        <Loader className="size-6 animate-spin text-muted-foreground" />
+      </div>
+    );
+  }
+
+  return (
+    <div className="h-full flex flex-col gap-y-8 items-center justify-center bg-white p-8 rounded-lg shadow-md">
+      <Image src={"/testLogo.svg"} width={160} height={160} alt="Bird logo" />
+      <div className="flex flex-col gap-y-4 items-center justify-center max-w-md">
+        <div className="flex flex-col gap-y-2 items-center justify-center">
+          <h1 className="text-2xl font-bold">Join {data?.name}</h1>
+          <p className="text-muted-foreground">Enter workspace code to join</p>
+        </div>
+        <VerificationInput
+          onComplete={handleComplete}
+          length={6}
+          classNames={{
+            container: cn(
+              "flex gap-x-2",
+              isPending && "opacity-50 cursor-not-allowed"
+            ),
+            character:
+              "uppercase h-auto rounded-md border border-gray-300 flex items-center justify-center text-lg font-medium text-gray-500",
+            characterInactive: "bg-muted",
+            characterSelected: "bg-white text-black",
+            characterFilled: "bg-white text-black",
+          }}
+          autoFocus
+        />
+      </div>
+      <div className="flex gap-x-4">
+        <Button size="lg" variant="outline" asChild>
+          <Link href="/">Back to gome</Link>
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default JoinPage;
