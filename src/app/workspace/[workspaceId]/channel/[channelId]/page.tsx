ea721e258c83b74528b51151eb51b56245f979e9
zWellingtonLima <0wellingtonlima1@gmail.com>
2024-10-15 23:35:59 +0100

    feat: added header and some renders state to channel page

--- src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/page.tsx
@@ -1,5 +1,40 @@
+"use client";
+
+import { Loader, TriangleAlert } from "lucide-react";
+
+import { useGetChannel } from "@/features/channels/api/use-get-individual-channel";
+import { useChannelId } from "@/hooks/use-channel-id";
+import { Header } from "./header";
+
 const ChannelIdPage = () => {
-  return <div></div>;
+  const channelId = useChannelId();
+
+  const { data: channel, isLoading: channelLoading } = useGetChannel({
+    id: channelId,
+  });
+
+  if (channelLoading) {
+    return (
+      <div className="h-full flex-1 flex items-center justify-center">
+        <Loader className="animate-spin size-5 text-muted-foreground" />
+      </div>
+    );
+  }
+
+  if (!channel) {
+    return (
+      <div className="h-full flex-1 gap-y-2 flex flex-col items-center justify-center">
+        <TriangleAlert className="size-5 text-muted-foreground" />
+        <span className="text-sm text-muted-foreground">Channel not found</span>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex flex-col h-full">
+      <Header title={channel.name} />
+    </div>
+  );
 };
 
 export default ChannelIdPage;
