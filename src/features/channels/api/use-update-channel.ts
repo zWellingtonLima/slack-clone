70e4f1f9afe1281a48dc80bc7db3883906a724d2
zWellingtonLima <0wellingtonlima1@gmail.com>
2024-10-20 12:37:33 +0100

    fix: fixed type RequestType

--- src/features/channels/api/use-update-channel.ts
+++ src/features/channels/api/use-update-channel.ts
@@ -4,7 +4,7 @@ import { useCallback, useMemo, useState } from "react";
 import { api } from "../../../../convex/_generated/api";
 import { Id } from "../../../../convex/_generated/dataModel";
 
-type RequestType = { name: string; workspaceId: Id<"channels"> };
+type RequestType = { name: string; id: Id<"channels"> };
 type ResponseType = Id<"channels"> | null;
 
 type Options = {
